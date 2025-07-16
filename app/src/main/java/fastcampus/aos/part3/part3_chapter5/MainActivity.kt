1d0ff375e5c4217d6485962f3fd179f158dc9b80
ParkHyunSu99 <phs0767@pineone.com>
2025-07-16 16:45:13 +0900

    검색 UI 그리기 1

--- app/src/main/java/fastcampus/aos/part3/part3_chapter5/MainActivity.kt
+++ app/src/main/java/fastcampus/aos/part3/part3_chapter5/MainActivity.kt
@@ -1,20 +1,40 @@
 package fastcampus.aos.part3.part3_chapter5
 
 import android.os.Bundle
+import android.view.Menu
+import android.widget.SearchView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import fastcampus.aos.part3.part3_chapter5.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+            it.view = this@MainActivity
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.options_menu, menu)
+        (menu.findItem(R.id.search).actionView as SearchView).apply {
+            setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+
+                    return false
+                }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    return false
+                }
+            })
+        }
+        return true
     }
 }
